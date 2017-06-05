ba12a2a82a2f2bafe4ddfcb67e3e7dad4632cd27
Casey Macaulay <caseyannemacaulay@gmail.com>
2017-06-05 14:11:13 -0600

    add only unread and only read button functionality

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -1,10 +1,26 @@
 $(document).ready(function(){
   $('#link-search').on('keyup', searchLinks);
+  $('#all-unread').on('click', showUnreadLinks)
+  $('#all-read').on('click', showReadLinks)
 });
 
 function searchLinks() {
   let $links = $('#my-links');
   let search = $('#link-search').val().toLowerCase();
+  $links.find(`div:contains(${search})`).show();
+  $links.find(`div:not(:contains(${search}))`).hide();
+};
+
+function showUnreadLinks() {
+  let $links = $('#my-links');
+  let search = "false"
+  $links.find(`div:contains(${search})`).show();
+  $links.find(`div:not(:contains(${search}))`).hide();
+};
+
+function showReadLinks() {
+  let $links = $('#my-links');
+  let search = "true"
   $links.find(`div:contains(${search})`).show();
   $links.find(`div:not(:contains(${search}))`).hide();
 };
